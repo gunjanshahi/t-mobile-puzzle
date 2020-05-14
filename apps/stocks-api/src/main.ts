7c3ac22c27e32e466c189077fc85ce3333026a33
gunjan <gunshahi1@publicisgroupe.net>
2020-05-14 13:44:06 +0530

    server caching implemented

--- apps/stocks-api/src/main.ts
+++ apps/stocks-api/src/main.ts
@@ -3,6 +3,7 @@
  * This is only a minimal backend to get started.
  **/
 import { Server } from 'hapi';
+import { priceCachePlugin } from './price-cache';
 
 const init = async () => {
   const server = new Server({
@@ -10,15 +11,7 @@ const init = async () => {
     host: 'localhost'
   });
 
-  server.route({
-    method: 'GET',
-    path: '/',
-    handler: (request, h) => {
-      return {
-        hello: 'world'
-      };
-    }
-  });
+  await server.register({plugin: priceCachePlugin})
 
   await server.start();
   console.log('Server running on %s', server.info.uri);
--- apps/stocks/src/environments/environment.ts
+++ apps/stocks/src/environments/environment.ts
@@ -6,8 +6,8 @@ import { StocksAppConfig } from '@coding-challenge/stocks/data-access-app-config
 
 export const environment: StocksAppConfig = {
   production: false,
-  apiKey: '',
-  apiURL: 'https://sandbox.iexapis.com'
+  apiKey: 'Tpk_ee9799da3aab42cda7b7d493bc5eb824',
+  apiURL: ''
 };
 
 /*
